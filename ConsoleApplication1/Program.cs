c30297ecfb6d98e6f21d466682f75ec1a424d554
Daljit Singh <daljit22ji.ds@gmail.com>
2021-09-21 11:35:59 +0530

    First

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=22;  
+            string msg = i % 2 == 0 ? "Entered Number is an Even Number" : "Entered Number is an Odd Number";            
+            Console.Write(msg);
+            Console.Read();                      
+        }
+    }
+}
